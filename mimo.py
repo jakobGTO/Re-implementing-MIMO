a9169dc36f8b486ec341cab9c9917a9261f0a62d
jakobGTO <jakob-wow@live.se>
2021-10-27 11:23:17 +0200

    push

--- mimo.py
+++ mimo.py
@@ -1,9 +1,10 @@
 import tensorflow as tf
 from tensorflow.keras import Model
-from tensorflow.keras.layers import Dense, BatchNormalization, Activation, Conv2D, add, Input, Flatten, AveragePooling2D, Reshape, Permute
+from tensorflow.keras.layers import Dense, BatchNormalization, Activation, Conv2D, Add, Input, Flatten, AveragePooling2D, Reshape, Permute
+from tensorflow.keras.datasets import cifar10, cifar100
 from keras_multi_head import MultiHead
 
-class mimo_resnet2810(object):
+class MIMO_ResNet28_10(object):
     def __init__(self):
         pass
 
@@ -22,36 +23,34 @@ class mimo_resnet2810(object):
             tf.tensor
         """
 
-        x = inputs
-        y = inputs
+        x_skip,x = inputs, inputs
 
-        y = BatchNormalization(momentum=0.99, epsilon=0.001)(y)
-        y = Activation('relu')(y)
-        y = Conv2D(filters=filters,strides=strides, kernel_size=3, padding='same', use_bias=False, kernel_initializer='he_normal')(y)
-        y = BatchNormalization(momentum=0.99, epsilon=0.001)(y)
-        y = Activation('relu')(y)
-        y = Conv2D(filters=filters, strides=1, kernel_size=3, padding='same', use_bias=False, kernel_initializer='he_normal')(y)
+        x = BatchNormalization(momentum=0.99, epsilon=0.001)(x)
+        x = Activation('relu')(x)
+        x = Conv2D(filters=filters,strides=(strides,strides), kernel_size=(3,3), padding='same', use_bias=False, kernel_initializer='he_normal')(x)
+        x = BatchNormalization(momentum=0.99, epsilon=0.001)(x)
+        x = Activation('relu')(x)
+        x = Conv2D(filters=filters, strides=(1,1), kernel_size=(3,3), padding='same', use_bias=False, kernel_initializer='he_normal')(x)
 
-        if not x.shape.is_compatible_with(y.shape):
-            x = Conv2D(filters=filters, strides=strides, kernel_size=1, padding='same', use_bias=False, kernel_initializer='he_normal')(x)
+        if not x.shape.is_compatible_with(x_skip.shape):
+            x_skip = Conv2D(filters=filters, strides=(strides,strides), kernel_size=(1,1), padding='same', use_bias=False, kernel_initializer='he_normal')(x_skip)
 
-        x = add([x,y])
+        x = Add()([x,x_skip])
 
         for i in range(n_blocks-1):
-            x = x
-            y = x
+            x_skip,x = x,x
 
-            y = BatchNormalization(momentum=0.99,epsilon=0.001)(y)
-            y = Activation('relu')(y)
-            y = Conv2D(filters=filters,strides=1, kernel_size=3, padding='same',use_bias=False, kernel_initializer='he_normal')(y)
-            y = BatchNormalization(momentum=0.99,epsilon=0.001)(y)
-            y = Activation('relu')(y)
-            y = Conv2D(filters=filters,strides=1, kernel_size=3, padding='same',use_bias=False, kernel_initializer='he_normal')(y)
+            x = BatchNormalization(momentum=0.99,epsilon=0.001)(x)
+            x = Activation('relu')(x)
+            x = Conv2D(filters=filters,strides=(1,1), kernel_size=(3,3), padding='same',use_bias=False, kernel_initializer='he_normal')(x)
+            x = BatchNormalization(momentum=0.99,epsilon=0.001)(x)
+            x = Activation('relu')(x)
+            x = Conv2D(filters=filters,strides=(1,1), kernel_size=(3,3), padding='same',use_bias=False, kernel_initializer='he_normal')(x)
 
-            if not x.shape.is_compatible_with(y.shape):
-                x = Conv2D(filters=filters, strides=strides, kernel_size=1, padding='same', use_bias=False, kernel_initializer='he_normal')(x)
+            if not x.shape.is_compatible_with(x_skip.shape):
+                x_skip = Conv2D(filters=filters, strides=(strides,strides), kernel_size=(1,1), padding='same', use_bias=False, kernel_initializer='he_normal')(x_skip)
 
-            x = add([x,y])
+            x = Add()([x,x_skip])
 
         return x
 
@@ -63,7 +62,7 @@ class mimo_resnet2810(object):
         Build ResNet-28-10 Model
 
         args:
-            input_shape: tf.tensor. Input shape (M, w, h, channels)
+            input_shape: tf.tensor. Input shape (M, img width, img height, channels)
             K: int. number of classes of the dataset 
             M: int. size of ensemble
         returns:
@@ -75,7 +74,7 @@ class mimo_resnet2810(object):
         # where dim_1 = size of ensemble, dim_2 = width, dim_3 = heigh, dim_4 = channels
         x = Permute([2,3,4,1])(inputs)
         x = Reshape(input_shape[1:-1] + [input_shape[-1] * M])(x)
-        x = Conv2D(filters=16, strides=1, kernel_size=3, padding='same', use_bias=False, kernel_initializer='he_normal')(x)
+        x = Conv2D(filters=16, strides=(1,1), kernel_size=(3,3), padding='same', use_bias=False, kernel_initializer='he_normal')(x)
 
         # We fit 4 resnet blocks to get a total network depth of 24
         # The filters are multiplied with 10 for the width multiplier
@@ -85,17 +84,30 @@ class mimo_resnet2810(object):
 
         x = BatchNormalization()(x)
         x = Activation('relu')(x)
+        x = AveragePooling2D(pool_size=8)(x)
         x = Flatten()(x)
 
         x = MultiHead(Dense(units=K, kernel_initializer='he_normal', activation=None), layer_num=M)(x)
 
         return Model(inputs=inputs, outputs=x)
 
+    @tf.function
+    def train_step():
+        
+        return None
+
+    def train():
+        return None
+
+    
+
 if __name__ == '__main__':
     print(tf.__version__)
 
-    model = mimo_resnet2810()
-    
-    model.build(input_shape=(5,5,5,3),K=5,M=5)
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-    print(model)
+    mimo = MIMO_ResNet28_10()
+    model = mimo.build(input_shape=(5,32,32,3),K=5,M=5)
+    model.summary()
+
+    print(x_train.shape)

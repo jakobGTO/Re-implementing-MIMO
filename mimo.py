cc4b7e459cf071b6997fbebef16b744e7007a3fe
DiogorPinheiro <diogomrpinheiro@gmail.com>
2021-10-30 12:40:49 +0200

    Added train and test steps

--- mimo.py
+++ mimo.py
@@ -2,11 +2,15 @@ import tensorflow as tf
 from tensorflow.keras import Model
 from tensorflow.keras.layers import Dense, BatchNormalization, Activation, Conv2D, Add, Input, Flatten, AveragePooling2D, Reshape, Permute
 from tensorflow.keras.datasets import cifar10, cifar100
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.losses import sparse_categorical_crossentropy
 from keras_multi_head import MultiHead
+import numpy as np
 
-class MIMO_ResNet28_10(object):
+
+class MIMO_ResNet28_10(Model):
     def __init__(self):
-        pass
+        super(MIMO_ResNet28_10, self).__init__()
 
     def block(self, inputs, filters,  strides):
         """
@@ -23,30 +27,30 @@ class MIMO_ResNet28_10(object):
             tf.tensor
         """
 
-        x_skip,x = inputs, inputs
+        x_skip, x = inputs, inputs
 
         x = BatchNormalization(momentum=0.99, epsilon=0.001)(x)
         x = Activation('relu')(x)
-        x = Conv2D(filters=filters,strides=(strides,strides), kernel_size=(3,3), padding='same', use_bias=False, kernel_initializer='he_normal')(x)
+        x = Conv2D(filters=filters, strides=(strides, strides), kernel_size=(3, 3), padding='same', use_bias=False, kernel_initializer='he_normal')(x)
         x = BatchNormalization(momentum=0.99, epsilon=0.001)(x)
         x = Activation('relu')(x)
-        x = Conv2D(filters=filters, strides=(1,1), kernel_size=(3,3), padding='same', use_bias=False, kernel_initializer='he_normal')(x)
+        x = Conv2D(filters=filters, strides=(1, 1), kernel_size=(3, 3), padding='same', use_bias=False, kernel_initializer='he_normal')(x)
 
-        x_skip = Conv2D(filters=filters, strides=(strides,strides), kernel_size=(1,1), padding='same', use_bias=False, kernel_initializer='he_normal')(x_skip)
+        x_skip = Conv2D(filters=filters, strides=(strides, strides), kernel_size=(1, 1), padding='same', use_bias=False, kernel_initializer='he_normal')(x_skip)
 
-        x = Add()([x,x_skip])
+        x = Add()([x, x_skip])
 
         for i in range(3):
-            x_skip,x = x,x
+            x_skip, x = x, x
 
-            x = BatchNormalization(momentum=0.99,epsilon=0.001)(x)
+            x = BatchNormalization(momentum=0.99, epsilon=0.001)(x)
             x = Activation('relu')(x)
-            x = Conv2D(filters=filters,strides=(1,1), kernel_size=(3,3), padding='same',use_bias=False, kernel_initializer='he_normal')(x)
-            x = BatchNormalization(momentum=0.99,epsilon=0.001)(x)
+            x = Conv2D(filters=filters, strides=(1,1), kernel_size=(3, 3), padding='same', use_bias=False, kernel_initializer='he_normal')(x)
+            x = BatchNormalization(momentum=0.99, epsilon=0.001)(x)
             x = Activation('relu')(x)
-            x = Conv2D(filters=filters,strides=(1,1), kernel_size=(3,3), padding='same',use_bias=False, kernel_initializer='he_normal')(x)
+            x = Conv2D(filters=filters, strides=(1, 1), kernel_size=(3, 3), padding='same', use_bias=False, kernel_initializer='he_normal')(x)
 
-            x = Add()([x,x_skip])
+            x = Add()([x, x_skip])
 
         return x
 
@@ -69,9 +73,9 @@ class MIMO_ResNet28_10(object):
         inputs = Input(shape=input_shape)
         # dim_1 -> dim_2, dim_2 -> dim_3, dim_3 -> dim_4, dim_4 -> dim_1
         # where dim_1 = size of ensemble, dim_2 = width, dim_3 = heigh, dim_4 = channels
-        x = Permute([2,3,4,1])(inputs)
+        x = Permute([2, 3, 4, 1])(inputs)
         x = Reshape(input_shape[1:-1] + [input_shape[-1] * M])(x)
-        x = Conv2D(filters=16, strides=(1,1), kernel_size=(3,3), padding='same', use_bias=False, kernel_initializer='he_normal')(x)
+        x = Conv2D(filters=16, strides=(1, 1), kernel_size=(3, 3), padding='same', use_bias=False, kernel_initializer='he_normal')(x)
 
         # The filters are multiplied with 10 for the width multiplier
         x = self.block(x, filters=160, strides=1)
@@ -88,19 +92,63 @@ class MIMO_ResNet28_10(object):
         return Model(inputs=inputs, outputs=x)
 
     @tf.function
-    def train_step():
-        return None
+    def train_step(self, data):
+        ''' Adapted from https://keras.io/guides/customizing_what_happens_in_fit/ '''
+        x, y = data
+        with tf.GradientTape() as tape:
+            logits = self(x, training=True)
+            trainable_vars = self.trainable_variables
+            loss = self.compiled_loss(y, logits, trainable_vars)
+        gradients = tape.gradient(loss, trainable_vars)
+        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+        self.compiled_metrics.update_state(y, logits)
+        return {m.name: m.result() for m in self.metrics}
 
-    def fit():
-        return None
+    @tf.function
+    def test_step(self, data):
+        ''' Adapted from https://keras.io/guides/customizing_what_happens_in_fit/ '''
+        x, y = data
+        y_pred = self(x, training=False)
+        self.compiled_loss(y, y_pred, regularization_losses=self.losses)
+        self.compiled_metrics.update_state(y, y_pred)
+        return {m.name: m.result() for m in self.metrics}
 
-    
+
+def custom_loss(y_true, y_pred, trainable_variables):
+    ''' Loss function as described in Section 2 of original paper '''
+    negative_likelihood = tf.reduce_mean(
+                tf.reduce_sum(sparse_categorical_crossentropy(
+                    y_true, y_pred, from_logits=True), axis=1))
+    lossL2 = tf.add_n([tf.nn.l2_loss(v) for v in trainable_variables if 'bias' not in v.name]) * 3e-4
+    return negative_likelihood + lossL2
+
 
 if __name__ == '__main__':
     print(tf.__version__)
 
+    # Data handling
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    y_train = to_categorical(y_train, num_classes=10)
+    y_test = to_categorical(y_test, num_classes=10)
+    x_train = np.reshape(x_train, (50000, 3072))
+    x_test = np.reshape(x_test, (10000, 3072))
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= 255
+    x_test /= 255
+    train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    train_dataset = train_dataset.shuffle(buffer_size=1024).batch(512)
+
+    # Set optimizer, adapted from https://keras.io/api/optimizers/ and with
+    # hyperparameters described in Annex B of the original paper.
+    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+        initial_learning_rate=1e-2, decay_steps=10000, decay_rate=0.1)
+    optimizer = tf.keras.optimizers.SGD(
+        learning_rate=lr_schedule, nesterov=True)
 
     mimo = MIMO_ResNet28_10()
-    model = mimo.build(input_shape=(5,32,32,3),K=5,M=5)
+    model = mimo.build(input_shape=(5, 32, 32, 3), K=5, M=5)
     model.summary()
+    model.compile(optimizer=optimizer, loss=custom_loss)
+    model.fit(train_dataset, epochs=250)
+    model.evaluate(x_test, y_test)
